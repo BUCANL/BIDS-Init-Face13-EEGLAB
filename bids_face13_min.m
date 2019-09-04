fcd44073f9458909c94479fc7deefba5a9874f0a
Andesha <tk11br@brocku.ca>
2019-09-04 11:33:10 -0400

    Debugging script updated.

--- bids_face13_min.m
+++ bids_face13_min.m
@@ -1,12 +1,16 @@
 % Preface:
 
+% Global settings
+bidsOpt.FileExt = 'edf';
+bidsOpt.InteralUse = true;
+
 data(1).file = {'./sourcedata/merged/sub-s01_task-faceFO_eeg.set'};
 data(1).session = 1;
 data(1).run     = 1;
 
 % general information for dataset_description.json file
 % -----------------------------------------------------
-generalInfo.Name = 'faceO';
+generalInfo.Name = 'faceFO';
 
 README = sprintf('# Face13 Dataset\n\nData used for JofV Deconstructing the early visual electrocortical response to face and house stimuli:\nhttps://jov.arvojournals.org/article.aspx?articleid=2121634');
 
@@ -18,7 +22,9 @@ chanlocs = './sourcedata/misc/BioSemi_BUCANL_EEGLAB.sfp';
 % ---------------------------
 bids_export(data, 'targetdir', uigetdir, 'taskName', ...
     generalInfo.Name, 'gInfo', generalInfo,  ...
-    'chanlocs', chanlocs, 'README', README);
+    'chanlocs', chanlocs, 'README', README, 'bidsOpt',bidsOpt);
+
+disp('Done!');
 
 % bids_export(data, 'targetdir', '/Users/arno/temp/bids_meditation_export', 'taskName', ...
 %     'meditation', 'trialtype', trialTypes, 'gInfo', generalInfo, 'pInfo', pInfo, ...
