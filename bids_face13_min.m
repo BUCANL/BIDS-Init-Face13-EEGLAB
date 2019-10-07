3ab3d4b62eb75737a9949d4e140ff43ca8d63bcb
Andesha <tk11br@brocku.ca>
2019-10-07 14:24:33 -0400

    Added coordsystem and dataset description support.

--- bids_face13_min.m
+++ bids_face13_min.m
@@ -11,6 +11,12 @@ data(1).run     = 1;
 % general information for dataset_description.json file
 % -----------------------------------------------------
 generalInfo.Name = 'faceFO';
+generalInfo.License = 'ODbL (https://opendatacommons.org/licenses/odbl/summary/)';
+generalInfo.Authors = {'James A. Desjardins', 'Sidney J. Segalowitz'};
+generalInfo.HowToAcknowledge = 'Desjardins, J. A., & Segalowitz, S. J. (2013). Deconstructing the early visual electrocortical responses to face and house stimuli. Journal of Vision, 13(5), 22-22.';
+generalInfo.Funding = {'NSERC funding to Sidney Segalowitz'};
+generalInfo.ReferencesAndLinks = {'http://jov.arvojournals.org/article.aspx?articleid=2121634'};
+generalInfo.DatasetDOI = '10.1167/13.5.22';
 
 README = sprintf('# Face13 Dataset\n\nData used for JofV Deconstructing the early visual electrocortical response to face and house stimuli:\nhttps://jov.arvojournals.org/article.aspx?articleid=2121634');
 
@@ -38,11 +44,16 @@ tInfo.CapManufacturersModelName = '10032';
 % ---------------------
 chanlocs = './sourcedata/misc/BioSemi_BUCANL_EEGLAB.sfp';
            
+% coordsystem string
+% This string will be placed in each coordsystem.json
+% ---------------------
+coordsys = '{"EEGCoordinateSystem":"Other","EEGCoordinateUnits":"metres"}';
+           
 % call to the export function
 % ---------------------------
 bids_export(data, 'targetdir', uigetdir, 'taskName', ...
-    generalInfo.Name, 'gInfo', generalInfo,  ...
-    'chanlocs', chanlocs, 'README', README, 'bidsOpt',bidsOpt, 'tInfo', tInfo);
+    generalInfo.Name, 'gInfo', generalInfo, 'coordsys', coordsys, ...
+    'chanlocs', chanlocs, 'README', README, 'bidsOpt', bidsOpt,'tInfo',tInfo);
 
 disp('Done!');
 
