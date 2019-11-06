9c5ac36d2a486910cbc5fe3434f0671f3fd964dc
Andesha <tk11br@brocku.ca>
2019-11-06 10:23:19 -0500

    Bidsify scripts now accept jsons as inputs. See bids_face13_min.

--- bids_face13_min.m
+++ bids_face13_min.m
@@ -1,45 +1,21 @@
-% Preface:
-
 % Global settings
 bidsOpt.FileExt = 'edf';
 bidsOpt.InteralUse = true;
 
-data(1).file = {'./sourcedata/eeg/sub-s01_task-faceFO_eeg.set'};
-data(1).session = 1;
-data(1).run     = 1;
-data(1).subID = '999';
+% Input file specifications
+% -----------------------------------------------------
+data = loadjson('testData.json');
+data.byJson = true;
 
 % general information for dataset_description.json file
 % -----------------------------------------------------
-generalInfo.Name = 'faceFO';
-generalInfo.License = 'ODbL (https://opendatacommons.org/licenses/odbl/summary/)';
-generalInfo.Authors = {'James A. Desjardins', 'Sidney J. Segalowitz'};
-generalInfo.HowToAcknowledge = 'Desjardins, J. A., & Segalowitz, S. J. (2013). Deconstructing the early visual electrocortical responses to face and house stimuli. Journal of Vision, 13(5), 22-22.';
-generalInfo.Funding = {'NSERC funding to Sidney Segalowitz'};
-generalInfo.ReferencesAndLinks = {'http://jov.arvojournals.org/article.aspx?articleid=2121634'};
-generalInfo.DatasetDOI = '10.1167/13.5.22';
-
-README = sprintf('# Face13 Dataset\n\nData used for JofV Deconstructing the early visual electrocortical response to face and house stimuli:\nhttps://jov.arvojournals.org/article.aspx?articleid=2121634');
+generalInfo = loadjson('generalInfo.json');
+generalInfo.byJson = true;
 
 % Task information for xxxx-eeg.json file
 % -----------------------------------------------------
-tInfo.TaskName = 'FaceHouseCheck';
-tInfo.TaskDescription = 'Visual presentation of oval cropped face and house images both upright and inverted. Rare left or right half oval checkerboards were presetned as targets for keypress response.';
-tInfo.InstitutionName =  'Brock University';
-tInfo.InstitutionAddress = '500 Glenridge Ave, St.Catharines, Ontario';
-tInfo.EOGChannelCount =  7;
-tInfo.EMGChannelCount =  0;
-tInfo.MiscChannelCount =  0;
-tInfo.TriggerChannelCount = 0;
-tInfo.PowerLineFrequency = 60;
-tInfo.EEGPlacementScheme = 'Custom equidistant 128 channel BioSemi montage established in coordination with Judith Schedden McMaster Univertisy';
-tInfo.Manufacturer = 'BioSemi';
-tInfo.ManufacturersModelName = 'ActiveTwo';
-tInfo.HardwareFilters = 'n/a';
-tInfo.SoftwareFilters = 'n/a';
-tInfo.SoftwareVersions = 'NI ActiView 532-Lores';
-tInfo.CapManufacturer = 'ElectroCap International';
-tInfo.CapManufacturersModelName = '10032';
+tInfo = loadjson('tInfo.json');
+tInfo.byJson = true;
 
 % channel location file
 % ---------------------
@@ -49,7 +25,9 @@ chanlocs = './sourcedata/misc/BioSemi_BUCANL_EEGLAB.sfp';
 % This string will be placed in each coordsystem.json
 % ---------------------
 coordsys = '{"EEGCoordinateSystem":"Other","EEGCoordinateUnits":"metres"}';
-           
+
+README = sprintf('# Face13 Dataset\n\nData used for JofV Deconstructing the early visual electrocortical response to face and house stimuli:\nhttps://jov.arvojournals.org/article.aspx?articleid=2121634');
+
 % call to the export function
 % ---------------------------
 bids_export(data, 'targetdir', uigetdir, 'taskName', ...
